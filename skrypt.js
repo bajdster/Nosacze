5d4cc0fad779bdb03c0f380359210aef2d1669bd
Michał <38719617+bajdster@users.noreply.github.com>
2018-09-06 10:31:35 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,16 @@
+window.onload = function()
+{
+	var loguj = document.getElementById("loguj");
+	
+	loguj.onmouseover = function()
+	{
+		this.style.transform ="translateX(0px)";
+	}
+	loguj.onmouseout = function()
+	{
+		this.style.transform ="translateX(-280px)";
+	}
+}
+
+	
+
